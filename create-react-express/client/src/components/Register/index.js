87db774d892a1d6b7705d397540c7ec5350a7134
Tyler Larrabee <tyler.k.larrabee@gmail.com>
2019-08-13 15:30:33 -0700

    fix

--- create-react-express/client/src/components/Register/index.js
+++ create-react-express/client/src/components/Register/index.js
@@ -6,7 +6,7 @@ import Wrapper from "../Wrapper";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
-import Row from 'react-bootstrap/Row'
+// import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import { PASSWORD_MATCH } from '../../MessageBundle'
