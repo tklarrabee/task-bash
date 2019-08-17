96a3529c1976f9590b8c14d9da95e417ac992cb4
Gravitation <chisong@ucdavis.edu>
2019-08-17 11:35:59 -0700

    adding UI for board title

--- create-react-express/client/src/components/Boards/index.js
+++ create-react-express/client/src/components/Boards/index.js
@@ -13,20 +13,100 @@ export function BoardContainer(props) {
 export class Header extends Component {
   state = {
     boardName: "Board Name",
-    users: ["user1", "user2", "user3"]
+    users: ["user1", "user2", "user3"],
+    isInEditMode: false
   };
+
+  changeEditMode = () => {
+    this.setState({
+      isInEditMode: !this.state.isInEditMode
+    });
+    console.log("should go to edit mode now");
+  };
+
+  updateComponentValue = () => {
+    this.setState({
+      isInEditMode: false,
+      boardName: this.refs.theTextInput.value
+    });
+  };
+
+  renderEditView = () => {
+    return (
+      <div>
+        <input
+          type="text"
+          defaultValue={this.state.boardName}
+          ref="theTextInput"
+        />
+        <button onClick={this.updateComponentValue}>OK</button>
+        <button onClick={this.changeEditMode}>X</button>
+      </div>
+    );
+  };
+
+  renderDefaultView = () => {
+    return (
+      <div onDoubleClick={this.changeEditMode}>{this.state.boardName}</div>
+    );
+  };
+
   render() {
     return (
-      <div className="header">
-        <TextEditor />{" "}
-        <button className="btn btn-primary">
-          <i className="fa fa-star-o" />
-        </button>
-        <span style={{ color: "white", float: "right" }}>
-          {" "}
-          Users: {this.state.users}
-          <button className="btn btn-success">invite</button>
-        </span>
+      <div>
+        <div className="plx-card silver">
+          <div
+            className="pxc-bg"
+            style={{
+              backgroundImage: `url(https://i.gyazo.com/a846fc87cca5ebd3942ae1e038bb5083.png)`
+            }}
+          >
+            {" "}
+          </div>
+          <div className="pxc-avatar">
+            <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/GB/en/999/EP4312-CUSA07658_00-AV00000000000049/1536930670000/image?w=240&amp;h=240&amp;bg_color=000000&amp;opacity=100&amp;_version=00_09_000" />
+          </div>
+          <div className="pxc-stopper"> </div>
+          <div className="pxc-subcard">
+            <div className="pxc-title">
+              {this.state.isInEditMode
+                ? this.renderEditView()
+                : this.renderDefaultView()}
+            </div>
+            <div className="pxc-sub">
+              This probably should be called tagline instead
+            </div>
+            <div className="bottom-row">
+              <div className="pxc-info">
+                <div className="flags">
+                  <span>
+                    <img src="http://pollux.fun/images/flags/EN.png" />
+                  </span>
+                  <span>
+                    <img src="http://pollux.fun/images/flags/BR.png" />
+                  </span>
+                  <span>
+                    <img src="http://pollux.fun/images/flags/FR.png" />
+                  </span>
+                  <span>
+                    <img src="http://pollux.fun/images/flags/TR.png" />
+                  </span>
+                  <span>
+                    <img src="http://pollux.fun/images/flags/JP.png" />
+                  </span>
+                </div>
+              </div>
+              <div className="links">
+                <a className="shop">
+                  <i className="fas fa-shopping-bag"> </i>
+                </a>
+                <a className="shop">
+                  <i className="fas fa-shopping-bag"> </i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -39,9 +119,7 @@ export function Columns(props) {
       <button className="btn btn-warning" onClick={props.addCards}>
         <i className="fa fa-plus" />
       </button>
-      <div className="box">
-        {props.children}
-      </div>
+      <div className="box">{props.children}</div>
     </div>
   );
 }
@@ -69,8 +147,7 @@ export function EmptyColumm(props) {
       <button className="btn btn-warning" onClick={props.addColumns}>
         <i className="fa fa-plus" />
       </button>
-      <div className="box">
-      </div>
+      <div className="box" />
     </div>
   );
 }
--- create-react-express/client/src/pages/Board.js
+++ create-react-express/client/src/pages/Board.js
@@ -9,7 +9,7 @@ const data = {
       title: 'Planned Tasks',
       label: '',
       cards: [
-        {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+        {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
         {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
       ]
     },
@@ -27,7 +27,7 @@ export default class App extends React.Component {
     return (
       <div>
       <Header/>
-    <Board data={data} draggable collapsibleLanes editable editLaneTitle canAddLanes/>
+    <Board data={data} draggable editable editLaneTitle canAddLanes/>
     </div>
     )
   }
